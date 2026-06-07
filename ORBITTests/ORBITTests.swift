e3ef04b4cef41505a0b50adfd69f5e119f353214
Nikhil Gour <futureworks.nik@gmail.com>
2026-06-08 03:11:00 +0530

    Initial Commit

---
+++ ORBITTests/ORBITTests.swift
@@ -0,0 +1,19 @@
+//
+//  ORBITTests.swift
+//  ORBITTests
+//
+//  Created by Nikhil Gour on 08/06/26.
+//
+
+import Testing
+@testable import ORBIT
+
+struct ORBITTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
